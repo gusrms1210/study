b80b47ce156ebd260d8cbd036005c4e53efbf788
park <gusrms@naver.com>
2017-06-26 13:44:16 +0900

    added "Tell her World"

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class Hello{
 		System.out.println("Hello world");
 		System.out.println("Hello world2");
 		System.out.println("Tell your World");
+		System.out.println("Tell her World");
 	}
 }
